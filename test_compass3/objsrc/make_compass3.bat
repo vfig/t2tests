d9df6b410e59833a6209bc7b45639d917ebb891d
Andy Durdin <me@andy.durdin.net>
2021-08-26 18:03:45 +0100

    bsp -N resolves the intersection issues! and setting the right Sim flags resolves the joints not animating issues! :D

--- test_compass3/objsrc/make_compass3.bat
+++ test_compass3/objsrc/make_compass3.bat
@@ -1,5 +1,5 @@
 @echo off
-bsp compass3.e compass3.bin -l0 -V -A1
+bsp compass3.e compass3.bin -N -l0 -V
 copy /y compass3.bin ..\obj\
 copy /y cstar1.png ..\obj\txt\
 copy /y cstar2.png ..\obj\txt\
