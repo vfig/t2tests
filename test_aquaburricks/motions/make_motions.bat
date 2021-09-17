935651e7eab5e7db200cf4fab69ba466323e80e7
Andy Durdin <me@andy.durdin.net>
2021-09-17 02:35:16 +0100

    first go at burrick swimming animations! they dont work right in-game yet, so trying to debug that next.

--- test_aquaburricks/motions/make_motions.bat
+++ test_aquaburricks/motions/make_motions.bat
@@ -2,3 +2,5 @@
 set cal=../mesh/burrick.cal
 set map=../mesh/burrick.map
 python bvh2mi.py burrheadbang.bvh %map% 7FFFF burrheadbang
+python bvh2mi.py bk111011s.bvh %map% 7FFFF bk111011s
+python bvh2mi.py bk111012s.bvh %map% 7FFFF bk111012s
