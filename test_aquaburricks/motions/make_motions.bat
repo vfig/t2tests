9d71fcffc4bf5cddf46cd9540d56100850cbeb1b
Andy Durdin <me@andy.durdin.net>
2021-09-17 03:23:42 +0100

    use a single motion instead of left/right foot pairs for swimming, because in my animation the feet dont end up touching the ground - (which breaks both the motion pair system and foot sounds)

--- test_aquaburricks/motions/make_motions.bat
+++ test_aquaburricks/motions/make_motions.bat
@@ -2,5 +2,4 @@
 set cal=../mesh/burrick.cal
 set map=../mesh/burrick.map
 python bvh2mi.py burrheadbang.bvh %map% 7FFFF burrheadbang
-python bvh2mi.py bk111011s.bvh %map% 7FFFF bk111011s
-python bvh2mi.py bk111012s.bvh %map% 7FFFF bk111012s
+python bvh2mi.py bkswimfwd.bvh %map% 7FFFF bkswimfwd
