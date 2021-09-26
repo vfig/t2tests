f20b960dc5e5967dbd2e15b31dc96c6e120c22cb
Andy Durdin <me@andy.durdin.net>
2021-09-26 11:46:39 +0100

    start trying to rig the face!

--- test_arachnorig/motions/make_motions.bat
+++ test_arachnorig/motions/make_motions.bat
@@ -1,4 +1,4 @@
 @echo off
-set cal=../mesh/spidalb2.cal
+set cal=../mesh/face.cal
 set map=../mesh/spidey.map
-python bvh2mi.py bstotalbs.bvh %map% 1FFFFFFF bstotalbs
+python bvh2mi.py facelook.bvh %map% 1FFFFFFF facelook
