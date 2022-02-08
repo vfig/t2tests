a54762e98ab39524b865e8bfaceab5fe46981219
Andy Durdin <me@andy.durdin.net>
2022-02-08 05:06:53 +0000

    test_babyburricks: sandbox for creating baby burrick behaviours and motions.

---
+++ test_babyburricks/motions/make_motions.bat
@@ -0,0 +1,6 @@
+@echo off
+set cal=../mesh/burrick.cal
+set map=../mesh/burrick.map
+python bvh2mi.py burrheadbang.bvh %map% 7FFFF burrheadbang
+python bvh2mi.py bkswimfwd.bvh %map% 7FFFF bkswimfwd
+python bvh2mi.py bkswimidle.bvh %map% 7FFFF bkswimidle
