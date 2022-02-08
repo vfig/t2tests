2a6c15c06296c1545c5590afbb0fa530fab01b30
Andy Durdin <me@andy.durdin.net>
2022-02-08 11:19:56 +0000

    start adapting some human-with-sword motions to burricks: - bbk213265 (from bh213265), a short side-hop to the left.

---
+++ test_babyburricks/motions/bvh2mi.bat
@@ -0,0 +1,15 @@
+@echo off
+if "%~1"=="" goto usage
+if "%~2"=="" goto makefilename
+set OUTPUT="%~2"
+goto run
+
+:usage
+echo "bvh2mi input.bvh [output.mi]"
+exit /B 1
+
+:makefilename
+set OUTPUT="%~dpn1.mi"
+
+:run
+python bvh2mi.py %1 ..\mesh\burrick.map 7FFFF %OUTPUT%
---
+++ test_babyburricks/motions/mi2bvh.bat
@@ -0,0 +1,15 @@
+@echo off
+if "%~1"=="" goto usage
+if "%~2"=="" goto makefilename
+set OUTPUT="%~2"
+goto run
+
+:usage
+echo "mi2bvh input.mi [output.bvh]"
+exit /B 1
+
+:makefilename
+set OUTPUT="%~dpn1.bvh"
+
+:run
+python mi2bvh.py %1 ..\mesh\burrick.cal ..\mesh\burrick.map %OUTPUT%
